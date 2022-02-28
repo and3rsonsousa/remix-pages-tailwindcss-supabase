0580e24ff3eb1e7d51a5a9033c2a1fade3b86629
Anderson Sousa <and3rsonsousa@outlook.com>
2022-02-28 18:00:24 -0300

    init

---
+++ app/routes/index.tsx
@@ -0,0 +1,10 @@
+export default function Index() {
+	return (
+		<div className="p-16">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores,
+			asperiores! Voluptate fugit, dolor laudantium quos minus eligendi
+			assumenda amet velit dignissimos necessitatibus aut, voluptatum illo
+			ipsa commodi excepturi ullam labore?
+		</div>
+	);
+}
